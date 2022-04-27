60f53310c1042ac26a52898079c87be8f03c1693
isuay <irisau2@gmail.com>
2022-04-27 21:37:59 +0200

    Creado ejercicios JS

---
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,25 @@
+/*// Pregunta al usuario su nombre
+ const nombre = prompt('Cual es tu nombre');
+
+// Toma el nombre y lo muestra en pantalla
+ document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JavScript Moderno`;
+
+// Asigna el valor hacia la variable producto
+ const producto = 'Monitor 24 pulgadas';
+
+ console.time('Hola');
+
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+
+console.timeEnd('Hola');*/
+
+function hola() {
+    console.log('ok');
+    console.log('Hola');
+    console.log('Mundo');
+}
---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,26 @@
+// Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 Pulgadas';
+console.log(producto);
+
+// JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin un valor y asignarlo despues
+var disponible;
+disponible = true;
+disponible = false;
+
+// Inicializamos múltiples variables
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+// Las variables no se pueden iniciar con números
+// Reglas escritura de variables
+var nombreProducto;
+var nombre_producto_categoria_precio;
